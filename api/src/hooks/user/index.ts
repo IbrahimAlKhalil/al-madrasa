ec59a9dd930d843b4eefedda07bcfc5c67235b8a
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2021-12-13 10:22:02 +0600

    chore(api): finish update hook with await

--- api/src/hooks/user/index.ts
+++ api/src/hooks/user/index.ts
@@ -1,15 +1,15 @@
-import {commonDeleteMaster} from "../app/common-delete-master";
-import {commonCopyMaster} from "../app/common-copy-master";
-import {registerHook} from "../../utils/register-hook";
-import {omit} from "lodash";
+import {commonDeleteMaster} from '../app/common-delete-master';
+import {commonCopyMaster} from '../app/common-copy-master';
+import {registerHook} from '../../utils/register-hook';
+import {omit} from 'lodash';
 
 export default registerHook((hook) => {
 	hook.action('roles.create', commonCopyMaster);
 	hook.action(
 		'roles.update',
-		(meta, ctx) => {
+		async (meta, ctx) => {
 			meta.payload = omit(meta.payload, 'last_page');
-			commonCopyMaster(meta, ctx);
+			await commonCopyMaster(meta, ctx);
 		}
 	);
 	hook.action('roles.delete', commonDeleteMaster);
