9b1be2e8bd34d6468f251597c6a18e3a6cf6e9d4
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2021-12-14 17:50:53 +0600

    chore(api): remove cli directory

--- api/src/storage.ts
+++ api/src/storage.ts
@@ -1,7 +1,4 @@
 import { LocalFileSystemStorage, Storage, StorageManager, StorageManagerConfig } from '@directus/drive';
-import { AzureBlobWebServicesStorage } from '@directus/drive-azure';
-import { GoogleCloudStorage } from '@directus/drive-gcs';
-import { AmazonWebServicesS3Storage } from '@directus/drive-s3';
 import env from './env';
 import { getConfigFromEnv } from './utils/get-config-from-env';
 import { toArray } from '@directus/shared/utils';
@@ -57,14 +54,5 @@ function registerDrivers(storage: StorageManager) {
 }
 
 function getStorageDriver(driver: string) {
-	switch (driver) {
-		case 'local':
-			return LocalFileSystemStorage;
-		case 's3':
-			return AmazonWebServicesS3Storage;
-		case 'gcs':
-			return GoogleCloudStorage;
-		case 'azure':
-			return AzureBlobWebServicesStorage;
-	}
+	return LocalFileSystemStorage;
 }
