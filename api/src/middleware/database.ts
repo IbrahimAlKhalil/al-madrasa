f9f9a084d3fdbd5d89317052d1b7a59c5df14094
Ibrahim Al Khalil <hm.ibrahimalkhalil@gmail.com>
2022-01-05 11:08:43 +0600

    chore(api): keep context specific qmmsoft db at request object

--- api/src/middleware/database.ts
+++ api/src/middleware/database.ts
@@ -7,9 +7,10 @@ const schema: RequestHandler = asyncHandler(async (req, res, next) => {
 
 	const institute = await req.masterDB.from('institute')
 		.where('domain', req.hostname.replace('www.', ''))
-		.select('db_name')
+		.select('db_name', 'qmmsoft_qb')
 		.first();
 
+	req.qmmsoftDB = institute?.qmmsoft_db ?? null;
 	req.knex = getDatabase(req.query.al_mad_app ?? req.cookies['al-mad-app'] ?? req.header('X-Al-Mad-App') ?? institute?.db_name ?? 'master') ?? req.masterDB;
 
 	return next();
