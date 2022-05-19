f23a2d6a71b78b4a1eae862989ccce2f1cc5050a
MMR Ahmad <info@mmrahmad.com>
2022-05-19 12:41:16 +0600

    build(theme): update nextjs config

--- themes/al-munir/next.config.js
+++ themes/al-munir/next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-    reactStrictMode: true,
-    eslint: {ignoreDuringBuilds: true},
-    typescript: {
-        tsconfigPath: './tsconfig.json',
-    }
+  reactStrictMode: true,
+  eslint: { ignoreDuringBuilds: true },
+  typescript: {
+    tsconfigPath: './tsconfig.json',
+  },
 };
